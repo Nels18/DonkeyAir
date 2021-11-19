2863d89c040db0339e22f11775fcd6872565d19f
Nelson Belgarde <nelsonbelgarde@gmail.com>
2021-11-19 02:09:04 +0100

    Create form for search booking with autocompletion of city

---
+++ get-airports.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once '_connect.php';
+$pdo = new \PDO(DSN, USER, PASS);
+
+$query = "SELECT name, code, country_name FROM airport;";
+$statement = $pdo->query($query);
+$airports = $statement->fetchAll(PDO::FETCH_ASSOC);
+$airportsList = json_encode($airports);
+echo $airportsList;
+// foreach ($airports as $airport) {
+
+//     echo  $airport['code']. " : " . $airport['name'] . " - " . $airport['country_name'] ."<br>";
+// }
