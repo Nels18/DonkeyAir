6a6b8a0b1c1687bbe4494ca0be2dafb31f440f02
saidi mbelo <sad@MacBook-Pro-5.local>
2021-11-25 17:37:08 +0100

    Create option fly

---
+++ inc/init.inc.php
@@ -0,0 +1,17 @@
+<?php
+// session_start();
+require_once '../lib/Database.php';
+// $pdo = new \PDO(DSN, USER, PASS);
+
+// $query = "SELECT name, code, country_name FROM airport;";
+// $statement = $pdo->query($query);
+// $airports = $statement->fetchAll(PDO::FETCH_ASSOC);
+// $airportsList = json_encode($airports);
+// echo $airportsList;
+
+
+
+// foreach ($airports as $airport) {
+
+//     echo  $airport['code']. " : " . $airport['name'] . " - " . $airport['country_name'] ."<br>";
+// }
